1e3be54a3a2552fcd7fcc130001357a14e770a2b
Akshithadogga <117561482+Akshithadogga@users.noreply.github.com>
2023-11-09 11:47:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	const form=document.getElementById("form1");
+	let allelements=form.getElementsByTagName("input");
+    let f_name=allelements[0].value;
+    let surname=allelements[1].value;
+    alert(f_name+" "+surname);
 }
